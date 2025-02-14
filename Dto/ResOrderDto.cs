56ecb02c1239b98f0807c59a6e6253abee692070
RamaRehawe <ramarehawe51@gmail.com>
2025-02-14 03:47:00 +0300

    Get All Customer Order

---
+++ Dto/ResOrderDto.cs
@@ -0,0 +1,18 @@
+﻿namespace FuelGo.Dto
+{
+    public class ResOrderDto
+    {
+        public DateTime Date { get; set; }
+        public string OrderNumber { get; set; }
+        public string LocationDescription { get; set; }
+        public string NeighborhoodName { get; set; }
+        public string FuelTypeName { get; set; }
+        public double OrderedQuantity { get; set; }
+        public double? Price { get; set; }
+        public double? FinalQuantity { get; set; }
+        public double? FinalPrice { get; set; }
+        public string? CustomerCarBrand { get; set; }
+        public string? CustomerApartmentName { get; set; }
+        public string AuthCode { get; set; }
+    }
+}
