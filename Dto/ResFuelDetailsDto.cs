910a21bfe4b76aac7c986730f8904a8d07db938d
RamaRehawe <ramarehawe51@gmail.com>
2025-03-13 18:10:19 +0300

    Get Fuel Details API

---
+++ Dto/ResFuelDetailsDto.cs
@@ -0,0 +1,10 @@
+﻿namespace FuelGo.Dto
+{
+    public class ResFuelDetailsDto
+    {
+        public int Id { get; set; }
+        public string FuelTypeName { get; set; }
+        public string CenterName { get; set; }
+        public double Price { get; set; }
+    }
+}
