e767e774f7ca4ecefaf412cca594a66822c45c11
RamaRehawe <ramarehawe51@gmail.com>
2025-04-21 22:27:29 +0300

    edit the lat and long in get pending order

--- Dto/ResPendingOrdersDto.cs
+++ Dto/ResPendingOrdersDto.cs
@@ -4,8 +4,8 @@
     {
         public DateTime Date { get; set; }
         public string OrderNumber { get; set; }
-        public double Lat { get; set; }
-        public double Long { get; set; }
+        public double? CustomerLat { get; set; }
+        public double? CustomerLong { get; set; }
         public string LocationDescription { get; set; }
         public int NeighborhoodId { get; set; }
         public int FuelTypeId { get; set; }
