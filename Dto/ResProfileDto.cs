ae5e33c1cdb5988f5e13b07f3af8264b4c6bbfcd
RamaRehawe <ramarehawe51@gmail.com>
2025-06-23 17:41:52 +0300

    Get Profile

---
+++ Dto/ResProfileDto.cs
@@ -0,0 +1,9 @@
+﻿namespace FuelGo.Dto
+{
+    public class ResProfileDto
+    {
+        public string Name { get; set; }
+        public string Phone { get; set; }
+        public string Email { get; set; }
+    }
+}
