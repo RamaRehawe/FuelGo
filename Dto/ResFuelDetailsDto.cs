b29bda2bc3811ba86bec1abeb00aa21a3c72ff85
RamaRehawe <ramarehawe51@gmail.com>
2025-07-01 21:19:16 +0300

    edit FuelTypeId instead of Id

--- Dto/ResFuelDetailsDto.cs
+++ Dto/ResFuelDetailsDto.cs
@@ -2,7 +2,7 @@
 {
     public class ResFuelDetailsDto
     {
-        public int Id { get; set; }
+        public int FuelTypeId { get; set; }
         public string FuelTypeName { get; set; }
         public string CenterName { get; set; }
         public double Price { get; set; }
